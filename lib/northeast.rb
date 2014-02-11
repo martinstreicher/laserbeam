cf387a519abeb387a182eb099bb5f263aaf044d8
Martin Streicher <martin.streicher@gmail.com>
2014-02-10 20:40:00 -0500

    Fix the northeast prism to meet the spec.

--- lib/northeast.rb
+++ lib/northeast.rb
@@ -1,3 +1,6 @@
+# A "/" mirror reflects an eastward beam to the north
+# (and vice versa), and a westward beam to the south (and vice versa).
+
 class Northeast < Optic
   def alter_x
     -1
@@ -5,5 +8,11 @@ class Northeast < Optic
 
   def alter_y
     1
+  end
+
+  def effect(previous_optic = nil)
+    new_x = x - previous_optic.alter_y
+    new_y = y - previous_optic.alter_x
+    [new_x, new_y]
   end
 end
